19a344917e9506660ddda04c33ae949b8ac428b5
Scott Kelly <scott@Scotts-MacBook-Pro.local>
2025-07-01 16:38:21 -0600

    added semaphore to limit requests

--- challenges/fetch_multiple_pokemon.py
+++ challenges/fetch_multiple_pokemon.py
@@ -8,30 +8,34 @@ from fetch_pokemon import print_json
 def format_endpoint_url(pokemon_name: str) -> str:
     return f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}'
 
-async def fetch_pokemon_data(session, endpoint) -> dict[str, Any] | None:
-    try:
-        async with session.get(endpoint) as resp: # add error handling
-            if resp.status == 200:
-                return await resp.json()
-            elif resp.status == 404:
-                # raise Exception(f'pokemon not found: {endpoint}')
-                print(f'pokemon not found: {endpoint}')
-            else:
-                # raise Exception(f'failed to get request: {endpoint}')
-                print(f'failed to get request: {endpoint}')
-            
-    except aiohttp.ClientError as e:
-        # raise Exception(f'client error {e}')
-       print(f'client error {e}')
-    
-    return None
+async def fetch_pokemon_data(semaphore, session, endpoint) -> dict[str, Any] | None:
+    async with semaphore:
+        try:
+            async with session.get(endpoint) as resp: # add error handling
+                if resp.status == 200:
+                    return await resp.json()
+                elif resp.status == 404:
+                    # raise Exception(f'pokemon not found: {endpoint}')
+                    print(f'pokemon not found: {endpoint}')
+                else:
+                    # raise Exception(f'failed to get request: {endpoint}')
+                    print(f'failed to get request: {endpoint}')
+                
+        except aiohttp.ClientError as e:
+            # raise Exception(f'client error {e}')
+            print(f'client error {e}')
+        
+        return None
 
 
 async def fetch_multiple_pokemon(session, pokemon_list: list) -> list[dict[str, Any]]:
+    semaphore = asyncio.Semaphore(5) # limit to 5 concurrent requests
+
     tasks = [asyncio.create_task(
-                fetch_pokemon_data(session, format_endpoint_url(pokemon))
+                fetch_pokemon_data(semaphore, session, format_endpoint_url(pokemon))
             ) for pokemon in pokemon_list]
     
+
     result: list = await asyncio.gather(*tasks, return_exceptions=True)
 
     parsed_data = []
