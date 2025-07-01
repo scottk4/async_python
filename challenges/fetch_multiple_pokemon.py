7b7bd40d35a34aaa39e1d768ef302da1c2479507
Scott Kelly <scott@Scotts-MacBook-Pro.local>
2025-07-01 16:28:30 -0600

    fetches multiple pokemon with better error handling

--- challenges/fetch_multiple_pokemon.py
+++ challenges/fetch_multiple_pokemon.py
@@ -5,31 +5,40 @@ from typing import Any
 
 from fetch_pokemon import print_json
 
-def format_endpoint_url(pokemon_name: str):
+def format_endpoint_url(pokemon_name: str) -> str:
     return f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}'
 
-async def fetch_individual_pokemon(session, endpoint) -> list[dict[str, Any]]:
+async def fetch_pokemon_data(session, endpoint) -> dict[str, Any] | None:
     try:
         async with session.get(endpoint) as resp: # add error handling
             if resp.status == 200:
                 return await resp.json()
             elif resp.status == 404:
-                raise Exception(f'pokemon not found: {endpoint}')
+                # raise Exception(f'pokemon not found: {endpoint}')
+                print(f'pokemon not found: {endpoint}')
             else:
-                raise Exception(f'failed to get request: {endpoint}')
+                # raise Exception(f'failed to get request: {endpoint}')
+                print(f'failed to get request: {endpoint}')
             
     except aiohttp.ClientError as e:
-        raise Exception(f'client error {e}')
+        # raise Exception(f'client error {e}')
+       print(f'client error {e}')
+    
+    return None
 
-async def fetch_multiple_pokemon(session, pokemon_list: list):
+
+async def fetch_multiple_pokemon(session, pokemon_list: list) -> list[dict[str, Any]]:
     tasks = [asyncio.create_task(
-                fetch_individual_pokemon(session, format_endpoint_url(pokemon))
+                fetch_pokemon_data(session, format_endpoint_url(pokemon))
             ) for pokemon in pokemon_list]
     
-    result: list = await asyncio.gather(*tasks)
+    result: list = await asyncio.gather(*tasks, return_exceptions=True)
 
     parsed_data = []
     for response_data in result:
+        if isinstance(response_data, Exception) or response_data is None:
+            continue
+
         response_data = {'name': response_data['name'],
                          'height': response_data['height'],
                          'weight': response_data['weight'],
@@ -37,12 +46,13 @@ async def fetch_multiple_pokemon(session, pokemon_list: list):
         parsed_data.append(response_data)
     
     print(parsed_data)
+    return parsed_data
 
 
 async def main():
     
     async with aiohttp.ClientSession() as session:
-        await fetch_multiple_pokemon(session, ["pikachu", "charizard", "bulbasaur"])
+        await fetch_multiple_pokemon(session, ["pikachu2", "charizard", "bulbasaur"])
 
 
 if __name__ == "__main__":
